3e97187287358d49efeca4155ceec5979084a0e6
YOMA <email": null>
2017-02-27 12:43:42 +0700

    selesai buat bar

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const width = 750,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,11 +15,43 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', (rows) => {
+    redraw(rows)
+  })
+
 }
 
 // redraw function
 let redraw = (data) => {
+  let score = [];
+  for (var i = 0; i < data.length; i++) {
+    score[i] = Number(data[i].GoalsScored)
+  }
+  // console.log(score);
   // Your data to graph here
+  const yScale = d3.scaleLinear()
+  .domain([0,4])
+  .range([0,height])
+
+  const xScale = d3.scaleLinear()
+  .domain([0,data.length])
+  .range([0,width])
+
+  svg.selectAll('rect')
+      .data(score)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d, i) => {
+        return xScale(i)
+      })
+      .attr('y', (d) => {
+        return 300 - yScale(d)
+      })
+      .attr('width', (width/data.length)-2)
+      .attr('height', (d) => {
+        return d * 80
+      })
 
 }
 
