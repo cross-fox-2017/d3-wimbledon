ded6174345472f88d6b02d4f50ac635474847bce
Syanmil <syanmil@yahoo.com>
2017-02-27 11:41:53 +0700

    initialize d3

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* global d3 */
 
 // Our canvas
-const width = 750,
-  height = 300,
-  margin = 20
-marginLeft = 40
+const width = 750;
+const height = 300;
+const margin = 20;
+const marginLeft = 40;
 
 // Drawing area
 let svg = d3.select('#results')
@@ -14,13 +14,32 @@ let svg = d3.select('#results')
 
 // Data reloading
 let reload = () => {
-  // Your data parsing here...
+  let data = []
+  d3.tsv('afcw-results.tsv', (rows) => {
+    rows.forEach(function(row){
+      data.push(row.GoalsScored)
+    })
+    redraw(data)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
-
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (data, i) => {
+      return i * 22
+    })
+    .attr('y', (data) => {
+      return 300 - data
+    })
+    .attr('width', 20)
+    .attr('height', (data) => {
+      return data
+    })
 }
 
 reload()
