d1710bc36e7d7a03bc0cc3a9ac8877fdf29ebf63
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-27 12:43:35 +0700

    axis

--- script.js
+++ script.js
@@ -15,11 +15,68 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', function(rows){
+    redraw(rows)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
+  // data pashing from reload function
+  console.log("data");
+  console.log(data);
+  // data convert to score and assing into socre
+  var score = [];
+  for (var i = 0; i < data.length; i++) {
+    score[i] = Number(data[i].GoalsScored)
+  }
+  console.log(score);
+
+  // draw data score into html
+
+  var yScale = d3.scaleLinear()
+      .domain([0, d3.max([score])])
+      .range([0, height])
+
+  var xScale = d3.scaleLinear()
+      .domain([0, score.length])
+      .range([0, width])
+
+  // manual
+  // svg.selectAll('rect')
+  //   .data(score)
+  //   .enter()
+  //   .append('rect')
+  //   .attr('class', 'bar')
+  //   .attr('x', (d, i) => {
+  //     return i * 22
+  //   })
+  //   .attr('y', (d) => {
+  //     return 300 - d * 70
+  //   })
+  //   .attr('width', 15)
+  //   .attr('height', (d) => {
+  //     return d * 70
+  //   })
+
+  // using sclae
+  svg.selectAll('rect')
+    .data(score)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => {
+      console.log(i);
+      xScale(i)
+    })
+    .attr('y', (d) => {
+      console.log("data");
+      return height - yScale(d)
+    })
+    .attr('width', 15)
+    .attr('height', (d) => {
+      return yScale(d)
+    })
 
 }
 
